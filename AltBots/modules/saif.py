2ef83f3edbba50826cba9ab5ce46ae218d44bce3
Shrish1239 <160769232+Shrish1239@users.noreply.github.com>
2024-03-26 21:09:21 +0530

    Update saif.py

--- AltBots/modules/saif.py
+++ AltBots/modules/saif.py
@@ -5,15 +5,15 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_BUTTON = [
     [
-        Button.url(" 𝐌ᴜsɪᴄ ", "https://t.me/HINATA_N_BOT"),
-        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/Saif_Dictator")
+        Button.url(" 𝐌ᴜsɪᴄ ", "☔☔"),
+        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/Homosapienhu")
     ],
     [
-        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/SAIFALLBOT"),
-        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/SAIFHELPGC")
+        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/sudeokeliyeaajaobclog"),
+        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/sudeokeliyeaajaobclog")
     ],
     [
-        Button.url("𝐑ᴇᴘᴏ ", "github.com/SAIFDEAD/AXSPAMBOT"),
+        Button.url("𝐑ᴇᴘᴏ ", "CHLA JAA RANDI KE BACCHE ☔"),
       
     ]
 ]
@@ -36,13 +36,13 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐃ɪᴄᴛᴀᴛᴏʀ](https://t.me/Saif_Dictator)**\n\n"
-        TEXT += f"» **ᴅᴇᴀᴅ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
-        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        TEXT += f"» **my developer : [wandering soul](https://t.me/Homosapienhu)**\n\n"
+        TEXT += f"» **wandering bot version :** `M3.3`\n"
+        TEXT += f"» **python version :** `3.11.3`\n"
+        TEXT += f"» **telethon version :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://graph.org/file/eb514f6d4b35027f8fe0b.jpg",
+                    "https://telegra.ph/file/7020ac6ebd771d2a8d050.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
 )
